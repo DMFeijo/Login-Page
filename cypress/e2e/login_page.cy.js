e8b9ae034885bd093b204c6d5135cd8c95859ea8
DMFeijo <danielmourafeijo@gmail.com>
2023-04-25 18:13:28 -0300

    login page

--- cypress/e2e/login_page.cy.js
+++ cypress/e2e/login_page.cy.js
@@ -1,6 +1,39 @@
-describe('Pagina de login', () => {
-  it('passes', () => {
-    // Acessar a URL
-    cy.visit('http://the-internet.herokuapp.com/login')
+beforeEach(() => {
+  cy.visit('http://the-internet.herokuapp.com/login')
+  cy.url().should('include', '/login')
+});
+describe('Login', () => {
+  it('Login de usuário com sucesso', () => {
+  
+    cy.get('#username').type('tomsmith')
+    cy.get('#password').type('SuperSecretPassword!')
+    cy.get('.radius').click()
+    cy.contains('You logged into a secure area!')
   })
+  it('Login com usuário invalido', () => {
+
+    cy.get('#username').type('teste')
+    cy.get('#password').type('SuperSecretPassword!')
+    cy.get('.radius').click()
+    cy.contains('Your username is invalid!')
+  });
+  it('Login com senha invalida', () => {
+
+    cy.get('#username').type('tomsmith')
+    cy.get('#password').type('teste')
+    cy.get('.radius').click()
+    cy.contains('Your password is invalid!')
+  });
+  it('Login com usuário e senha invalido', () => {
+
+    cy.get('#username').type('teste')
+    cy.get('#password').type('teste')
+    cy.get('.radius').click()
+    cy.contains('Your username is invalid!')
+  });
+  it('Não preencher os campos Login e Senha', () => {
+
+    cy.get('.radius').click()
+    cy.contains('Your username is invalid!')
+  });
 })
